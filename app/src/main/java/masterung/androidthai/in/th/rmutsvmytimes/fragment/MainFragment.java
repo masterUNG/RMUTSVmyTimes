3b55c4b1d2b405dfe5d3422429742984d5536961
MasterUNG <phrombutr@gmail.com>
2018-01-25 13:35:53 +0700

    Get Value From Firebase Success

--- app/src/main/java/masterung/androidthai/in/th/rmutsvmytimes/fragment/MainFragment.java
+++ app/src/main/java/masterung/androidthai/in/th/rmutsvmytimes/fragment/MainFragment.java
@@ -6,6 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Map;
 
 import masterung.androidthai.in.th.rmutsvmytimes.R;
 
@@ -15,6 +24,42 @@ import masterung.androidthai.in.th.rmutsvmytimes.R;
 
 public class MainFragment extends Fragment{
 
+    //    Explicit
+    private TextView textView;
+    private DatabaseReference databaseReference;
+    private FirebaseDatabase firebaseDatabase;
+    private String myTimesString;
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+//        Initial View
+        textView = getView().findViewById(R.id.txtShowMyTimes);
+
+//        Get Data From Firebase
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                Map map = (Map) dataSnapshot.getValue();
+                myTimesString = String.valueOf(map.get("myTimes"));
+                textView.setText(myTimesString);
+
+
+            }   // onDataChange
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+    }   // Main Method
 
     @Nullable
     @Override
