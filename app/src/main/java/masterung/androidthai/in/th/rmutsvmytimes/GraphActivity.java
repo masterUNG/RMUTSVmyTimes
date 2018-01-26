fb39c31694ba27e5a966d158c1b7c8080fa3d536
MasterUNG <phrombutr@gmail.com>
2018-01-26 09:50:54 +0700

    Add GraphFragment Success

--- app/src/main/java/masterung/androidthai/in/th/rmutsvmytimes/GraphActivity.java
+++ app/src/main/java/masterung/androidthai/in/th/rmutsvmytimes/GraphActivity.java
@@ -3,11 +3,27 @@ package masterung.androidthai.in.th.rmutsvmytimes;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import masterung.androidthai.in.th.rmutsvmytimes.fragment.GraphFragment;
+
 public class GraphActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_graph);
-    }
-}
+
+//        Add Fragment
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.contentGraphFragment, new GraphFragment())
+                    .commit();
+
+        }
+
+
+
+    }   // Main Method
+
+
+}   // Main Class
